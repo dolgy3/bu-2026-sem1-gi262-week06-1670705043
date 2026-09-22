76eeaee1185b1a5cdb1d1be3aa9fd49b216a75e6
Warattaya <dolgy3sh0re@gmail.com>
2026-09-22 10:28:05 +0700

    Test

--- Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
+++ Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 namespace MidtermExam.Prob01
@@ -5,5 +6,24 @@ namespace MidtermExam.Prob01
     public class GameEntity
     {
         // TODO: Implement fields and methods according to Class Diagram
+
+        public string id;
+        private Vector3 position;
+        protected int health;
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void TakeDamage(int damage)
+        {
+
+        }
+
+        private void Move(Vector3 direction)
+        {
+
+        }
     }
 }
